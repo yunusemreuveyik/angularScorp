c29c82edcb71b76bb52068d894f3cf958eef64eb
Yunus Emre ÜVEYİK <yunusemre@kod.com.tr>
2021-08-04 13:15:31 +0300

    send request created

---
+++ src/app/models/contactRequest.ts
@@ -0,0 +1,7 @@
+export class contactRequest {
+    country_code:string;
+    email:string;
+    name:string;
+    phonenumber:string;
+    text:string;
+}
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
+import { contactRequest } from 'src/app/models/contactRequest';
 import { countries } from 'src/app/models/countries';
 import { userinfo } from 'src/app/models/userinfo';
 import { getUserInfo } from 'src/app/state/app.selectors';
@@ -14,10 +15,10 @@ import { appState } from 'src/app/state/app.state';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-  userinfo$:Observable<any>
-  contactForm:FormGroup
-  selectedCountry:any
-   countryList: countries[] = [
+  userinfo$: Observable<any>
+  contactForm: FormGroup
+  selectedCountry: any
+  countryList: countries[] = [
     { id: "TR", name: "Turkey" },
     { id: "US", name: "United States of America" },
     { id: "GB", name: "United Kingdom" },
@@ -29,49 +30,49 @@ export class ContactComponent implements OnInit {
   ]
   email: string;
   name: string;
-  filteredOptions:any
+  filteredOptions: any
   constructor(protected fb: FormBuilder,
-    private store: Store<{appStateOBJ:appState}>) { }
+    private store: Store<{ appStateOBJ: appState }>) { }
 
   ngOnInit(): void {
 
     this.filteredOptions = this.countryList
-   
+
     this.userinfo$ = this.store.select(getUserInfo)
-    this.userinfo$.subscribe((res:userinfo)=>{
+    this.userinfo$.subscribe((res: userinfo) => {
       console.log('data: ', res);
-      
+
       this.email = res.email
       this.name = res.name
 
       this.contactForm = this.fb.group({
-        title: new FormControl("",Validators.required),
-        message: new FormControl("",Validators.required),
-        name: new FormControl(this.name,Validators.required),
-        phone: new FormControl("",Validators.required),
-        country: new FormControl("",Validators.required),
-        email: new FormControl(this.email,[Validators.required, Validators.email]),
+        title: new FormControl("", Validators.required),
+        message: new FormControl("", Validators.required),
+        name: new FormControl(this.name, Validators.required),
+        phone: new FormControl("", Validators.required),
+        country: new FormControl("", Validators.required),
+        email: new FormControl(this.email, [Validators.required, Validators.email]),
       })
-      this.contactForm.get("country")!.valueChanges.subscribe(res=>{
+      this.contactForm.get("country")!.valueChanges.subscribe(res => {
         console.log(res)
         this.filterData(res)
       })
-      
+
     })
   }
 
-  private filterData(value:string){
+  private filterData(value: string) {
     const filterValue = value.toLowerCase()
     this.filteredOptions = this.countryList.filter(option => option.name?.toLowerCase().includes(filterValue))
   }
 
-  displayFn(subject:any){
+  displayFn(subject: any) {
     console.log('subject: ', subject);
-    
+
     return subject ? subject.name : undefined
   }
-  send(){
-    var request = {
+  send() {
+    var request: contactRequest = {
       name: this.contactForm.value.name,
       email: this.contactForm.value.email,
       phonenumber: this.contactForm.value.phone,
